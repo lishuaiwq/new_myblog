2d265eb43e6d23cde73a2e7f6e11a55158ffdc5e
李帅l <shui.li@qunarservers.com>
2019-01-16 13:41:11 +0800

    修改包名为lishuai

---
+++ blog/src/main/java/com/lishuai/service/CommentService.java
@@ -0,0 +1,17 @@
+package com.lishuai.service;
+
+
+
+import com.lishuai.po.Comment;
+
+import java.util.List;
+
+/**
+ * Created by limi on 2017/10/22.
+ */
+public interface CommentService {
+
+    List<Comment> listCommentByBlogId(Long blogId);
+
+    Comment saveComment(Comment comment);
+}
---
+++ blog/src/main/java/com/lishuai/service/UserService.java
@@ -0,0 +1,12 @@
+package com.lishuai.service;
+
+
+import com.lishuai.po.User;
+
+/**
+ * Created by limi on 2017/10/15.
+ */
+public interface UserService {
+
+    User checkUser(String username, String password);
+}
